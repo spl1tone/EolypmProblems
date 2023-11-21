d44220085d2fd6f7a082a9fc7cadc8dcef4f6f36
spl1tOne <levonvlad144@gmail.com>
2023-11-21 20:52:07 +0200

    completed

--- EolypmProblems/Program.cs
+++ EolypmProblems/Program.cs
@@ -5,6 +5,18 @@ internal class Program
     static void Main ()
     {
 
+        /*
+         Підрахувати кількість цифр цілого невід'ємного числа n.
+         Вхідні дані
+         Одне ціле невід'ємне число n (0 ≤ n ≤ 2*10^9).
+         Вихідні дані
+         Виведіть кількість цифр у числі n.
+         */
+
+        Console.WriteLine(Console.ReadLine().Length);
+
+        //Runtime
+        //100/100 38ms 24,555MB
     }
 }
 
