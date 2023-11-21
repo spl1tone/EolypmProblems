59b17f9807c184a059de4b96949281f0ada5797b
spl1tOne <levonvlad144@gmail.com>
2023-11-21 20:12:04 +0200

    completed

--- EolypmProblems/Program.cs
+++ EolypmProblems/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace EolypmProblems;
+﻿using System;
+
+namespace EolypmProblems;
 
 internal class Program
 {
     static void Main ()
     {
+        /*
+        Програма зчитує двоцифрове число і виводить через пропуск кожну цифру окремо.
 
+        Вхідні дані
+        Натуральне число на проміжку від 10 до 99 включно.
+        
+        Вихідні дані
+        Спочатку першу цифру числа і через пропуск другу.
+
+        */
+
+        var num = byte.Parse(Console.ReadLine());
+        Console.WriteLine($"{num / 10} {num % 10}");
+
+        //Runtime
+        // 100/100 39ms 24,672MB
     }
+
 }
 
