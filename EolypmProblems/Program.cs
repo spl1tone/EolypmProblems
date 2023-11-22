e0e4f518c77047b2104f6a47c8d67b1d2c6904c4
spl1tOne <levonvlad144@gmail.com>
2023-11-22 20:51:51 +0200

    2

--- EolypmProblems/Program.cs
+++ EolypmProblems/Program.cs
@@ -4,7 +4,25 @@ internal class Program
 {
     static void Main ()
     {
+        // x1 y1 r1, x2 y2 r2
 
+        var str = Console.ReadLine();
+        float[] circles = new float[6];
+
+        CirclesMassive(str, ref circles);
+
+        if (circles[0] == circles[3] && circles[1] == circles[4] && circles[2] == circles[5]) Console.WriteLine(1);
+
+
+    }
+
+    static void CirclesMassive (string str, ref float[] circles)
+    {
+        string[] newStr = str.Split(' ');
+
+        for (int i = 0; i < 6; i++) {
+            circles[i] = float.Parse(newStr[i]);
+        }
     }
 }
 
