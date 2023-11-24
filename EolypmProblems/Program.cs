98f6cffa4bc368ba5fd57cc3a6b3067772fcd591
spl1tOne <levonvlad144@gmail.com>
2023-11-24 17:34:25 +0200

    completed

--- EolypmProblems/Program.cs
+++ EolypmProblems/Program.cs
@@ -4,7 +4,32 @@ internal class Program
 {
     static void Main ()
     {
+        var input = Console.ReadLine().Split(' ');
 
+        ushort a = ushort.Parse(input[0]); // Робочі дні
+        ushort b = ushort.Parse(input[1]); // Вихідні дні
+        ushort n = ushort.Parse(input[2]); // Не працює в конкретний день
+        ushort m = ushort.Parse(input[3]); // День в який він виконує подвійну роботу
+        ushort k = ushort.Parse(input[4]); // Місяць
+
+        ushort currentDay = 1;
+        ushort result = 0;
+
+        while (currentDay <= k) {
+
+            for (var i = 1; i <= a && currentDay <= k; i++) {
+
+                if (currentDay == n) { result += 0; }
+                else if (currentDay == m) { result += 2; }
+                else { result += 1; }
+
+                currentDay++;
+            }
+
+            currentDay += b;
+        }
+
+        Console.WriteLine(result);
     }
 }
 
