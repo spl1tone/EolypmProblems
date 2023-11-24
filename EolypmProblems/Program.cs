3e29442968c12f0f775efbf33f8f6508ed64c3ea
spl1tOne <levonvlad144@gmail.com>
2023-11-24 14:07:59 +0200

    completed

--- EolypmProblems/Program.cs
+++ EolypmProblems/Program.cs
@@ -4,7 +4,28 @@ internal class Program
 {
     static void Main ()
     {
+        var bus = Num(Console.ReadLine());
+        var metro = Num(Console.ReadLine());
+        var taxi = Num(Console.ReadLine());
 
+        var m = Math.Min(bus, Math.Min(metro, taxi));
+
+        if (bus == m) { Console.WriteLine("Bus"); }
+        else if (metro == m) { Console.WriteLine("Metro"); }
+        else if (taxi == m) { Console.WriteLine("Taxi"); }
+        else if (bus == metro || bus == taxi) { Console.WriteLine("Bus"); }
+        else if (metro == taxi && metro < bus) { Console.WriteLine("Metro"); }
+        else { Console.WriteLine("Taxi"); }
+
+    }
+
+    static int Num (string input)
+    {
+        var res = 0;
+        int[] nums = input.Split(' ').Select(int.Parse).ToArray();
+        foreach (var n in nums) res += n;
+
+        return res;
     }
 }
 
