7b89044accc21435e5f9db076fbbfc4bd1704de8
spl1tOne <levonvlad144@gmail.com>
2023-11-24 10:58:49 +0200

    completed

--- EolypmProblems/Program.cs
+++ EolypmProblems/Program.cs
@@ -5,47 +5,45 @@ internal class Program
     static void Main ()
     {
         var input = Console.ReadLine();
-        int[] nums = input.Split(' ').Select(int.Parse).ToArray();
+        ushort[] nums = input.Split(' ').Select(ushort.Parse).ToArray();
 
-        var count = 0;
+        ushort count = 0;
 
         for (var i = nums[0]; i <= nums[1]; i++) {
-            IsPrime(i, false, ref count);
+            if (IsPrime(i) == true && IsPrime(ReverseNum(i)) == true) count++;
         }
 
         Console.WriteLine(count);
+
+        //Runtime
+        // 100/100 71ms 25,465MB
+
     }
 
-    static void IsPrime (int num, bool isRepeat, ref int count)
+    static bool IsPrime (ushort num)
     {
+        ushort count = 0;
+        if (num == 1)
+            return false;
 
-        var repeatCount = 0;
+        for (ushort i = 1; i <= num; i++) {
+            if (num % i == 0) count++;
+            if (count > 2) return false;
+        }
 
-        if (!isRepeat) {
-            for (var i = 1; i <= num; i++) {
-                if (num % i == 0) { repeatCount++; }
-                if (repeatCount > 2) break;
-            }
-            IsPrime(ReverseNum(num), true, ref count);
-        }
-        if (isRepeat) {
-            for (var i = 1; i <= num; i++) {
-                if (num % i == 0) { repeatCount++; }
-                if (repeatCount > 2) break;
-            }
-        }
+        return true;
     }
 
 
-    static int ReverseNum (int num)
+    static ushort ReverseNum (ushort num)
     {
-        var newNum = 0;
+        ushort newNum = 0;
         while (num > 0) {
-            var digit = num % 10;
-            newNum = newNum * 10 + digit;
+            ushort digit = (ushort)(num % 10);
+            newNum = (ushort)(newNum * 10 + digit);
             num /= 10;
         }
-        // Console.WriteLine(newNum);
+
         return newNum;
     }
 }
