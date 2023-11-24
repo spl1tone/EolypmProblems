9845cef4be6993456ddc3ecab966929f96804413
spl1tOne <levonvlad144@gmail.com>
2023-11-24 12:45:14 +0200

    completed

--- EolypmProblems/Program.cs
+++ EolypmProblems/Program.cs
@@ -4,6 +4,14 @@ internal class Program
 {
     static void Main ()
     {
+        var input = Console.ReadLine();
+        ushort[] nums = input.Split(' ').Select(ushort.Parse).ToArray();
+
+        Console.WriteLine(
+
+       ((nums[0] - nums[2] + (nums[1] - nums[3]) * 2) * 100) / (nums[0] + nums[1] * 2) >= 51 ? "YES" : "NO"
+
+            );
 
     }
 }
