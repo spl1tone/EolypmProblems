ba34c87fc149a6314e797e4119cc677ef7f94833
spl1tOne <levonvlad144@gmail.com>
2023-12-03 11:46:59 +0200

    100

--- EolypmProblems/Program.cs
+++ EolypmProblems/Program.cs
@@ -4,6 +4,24 @@ internal class Program
 {
     static void Main ()
     {
+        var str = Console.ReadLine().Split(' ');
+        var d1 = byte.Parse(str[0]);
+        var m1 = byte.Parse(str[1]);
+
+        var str1 = Console.ReadLine().Split(' ');
+        var d2 = byte.Parse(str1[0]);
+        var m2 = byte.Parse(str1[1]);
+
+        if (d1 + 13 > 30) {
+            d1 = (byte)(d1 + 13 - 30);
+            if (m1 % 12 == 0) m1 = 1;
+            else m1++;
+        }
+        else {
+            d1 += 13;
+        }
+
+        Console.WriteLine(d1 == d2 && m1 == m2 ? "Same birthday!" : "Not the same");
 
     }
 }
