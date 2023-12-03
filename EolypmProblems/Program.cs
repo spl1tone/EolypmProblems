9f32c46d5af19c6885930ab12c6ee964d9590319
spl1tOne <levonvlad144@gmail.com>
2023-12-03 11:21:48 +0200

    0

--- EolypmProblems/Program.cs
+++ EolypmProblems/Program.cs
@@ -9,10 +9,25 @@ internal class Program
         var n = ushort.Parse(Console.ReadLine());
         var m = ushort.Parse(Console.ReadLine());
 
-        //  Console.WriteLine((n - 1) * m + 1);
+        ushort res = 1;
 
+        while (n > 0 || m > 0) {
+            if (n > 1) {
+                n--;
+                res++;
+            }
+            else if (m > 1) {
+                m--;
+                res++;
+            }
+            else {
+                res++;
+                break;
+            }
+        }
+
+        Console.WriteLine(res);
     }
-
 
 }
 
