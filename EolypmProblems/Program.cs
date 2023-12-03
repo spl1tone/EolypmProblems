5bf542cc37b9307644b505a9b3ba8c6081f1faa2
spl1tOne <levonvlad144@gmail.com>
2023-12-03 10:31:29 +0200

    paused

--- EolypmProblems/Program.cs
+++ EolypmProblems/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace EolypmProblems;
+﻿using System.Collections.Generic;
+
+namespace EolypmProblems;
 
 internal class Program
 {
     static void Main ()
     {
+        var n = ushort.Parse(Console.ReadLine());
+        var m = ushort.Parse(Console.ReadLine());
+
+        //  Console.WriteLine((n - 1) * m + 1);
 
     }
+
+
 }
 
