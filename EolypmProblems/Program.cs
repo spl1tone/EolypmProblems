82527a72de2b45f424bb087789cb0bbda5c73e9f
spl1tOne <levonvlad144@gmail.com>
2023-12-03 10:04:42 +0200

    100

--- EolypmProblems/Program.cs
+++ EolypmProblems/Program.cs
@@ -4,7 +4,10 @@ internal class Program
 {
     static void Main ()
     {
+        var m = byte.Parse(Console.ReadLine());
+        var n = byte.Parse(Console.ReadLine());
 
+        Console.WriteLine(m - n * 3);
     }
 }
 
