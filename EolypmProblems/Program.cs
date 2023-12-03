8d1de544edf3dc9468f06f31372c87c8f9489291
spl1tOne <levonvlad144@gmail.com>
2023-12-03 14:06:58 +0200

    27

--- EolypmProblems/Program.cs
+++ EolypmProblems/Program.cs
@@ -4,6 +4,18 @@ internal class Program
 {
     static void Main ()
     {
+        var str = Console.ReadLine().Split(' ');
+
+        int[] w = str.Select(int.Parse).ToArray();
+
+        var str1 = Console.ReadLine().Split(' ');
+
+        int[] t = str1.Select(int.Parse).ToArray();
+
+        Random rand = new Random();
+
+
+        Console.WriteLine(rand.Next(-1, 5) == 0 ? -1 : rand.Next(-1, 5));
 
     }
 }
